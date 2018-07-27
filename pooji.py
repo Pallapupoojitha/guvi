e08f50174a76c1f076e8f6fc7fcfa21c9ff2c318
Pallapupoojitha <41770542+Pallapupoojitha@users.noreply.github.com>
2018-07-27 15:22:06 +0530

    Update pooji.py

--- pooji.py
+++ pooji.py
@@ -1,7 +1,7 @@
-n =int(raw_input())
+n = int(raw_input())
 if (n>0):
-print("Positive")
-elif(n<0):
-print("Negative")
+        print("Positive")
+elif (n<0):
+         print("Negative")
 else:
-print("Zero")
+          print("Zero")
