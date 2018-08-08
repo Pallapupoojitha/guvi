bc5f578b2613649fa52138c3a579b47181dfad04
Pallapupoojitha <41770542+Pallapupoojitha@users.noreply.github.com>
2018-08-08 15:54:42 +0530

    Rename pooji6.py to pooji7.py

---
+++ pooji7.py
@@ -0,0 +1,10 @@
+p=int(input("enter the number 1"))
+q=int(input("enter the number 2"))
+r=int(input("enter the number 3"))
+if(p>q):
+   if(p>r):
+     print(p)
+  elif(q>r):
+     print(q)
+  else:
+     print(c)
