c0607a16ba1ce786500e06e2cb84cf8a0b86e74f
Pallapupoojitha <41770542+Pallapupoojitha@users.noreply.github.com>
2018-08-11 12:48:23 +0530

    Update pooji13.py

--- pooji13.py
+++ pooji13.py
@@ -1,8 +1,8 @@
-number=int(input())
-if number > 1:
-             for i in range(2,number):
-                if(number % i)==0:
-                       print("no")
-                       break
+n=int(input())
+if(n>1):
+             for i in range(2,n):
+                         if(n%i)==0:
+                                   print("no")
+                                   break
                  else:
-                       print("yes")
+                                   print("yes")
