50b9733c516e2c699c7c68e6e441f5859aa484cf
Pallapupoojitha <41770542+Pallapupoojitha@users.noreply.github.com>
2018-08-13 15:08:23 +0530

    Update pooji18.py

--- pooji18.py
+++ pooji18.py
@@ -5,6 +5,6 @@ for num in range(m,n):
          while temp>0:
                  digit=temp%10
                  sum+=digit**3
-                 temp//10
+                 temp=temp//10
          if num==sum:
                         print num,
