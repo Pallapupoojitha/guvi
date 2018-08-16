708988e3cc57f27161d2770ecd23b1f78b3da07e
Pallapupoojitha <41770542+Pallapupoojitha@users.noreply.github.com>
2018-08-16 14:54:23 +0530

    Update pooji25.py

--- pooji25.py
+++ pooji25.py
@@ -1,4 +1,4 @@
-def median(1,n):
+def median(l,n):
        l.sort()
        if n%2==0:
                      return l[n/2]
@@ -6,4 +6,4 @@ def median(1,n):
                      return(l[n/2-1]+l[n/2])/2
 n=int(raw_input())
 i=[int(x) for  in raw_input().split()]
-print(median(1,n-1))
+print(median(l,n-1))
