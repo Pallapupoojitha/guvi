c8825a96bfb282c1048eed826ce54a24fec71c0f
Pallapupoojitha <41770542+Pallapupoojitha@users.noreply.github.com>
2018-08-16 15:57:25 +0530

    Update pooji30.py

--- pooji30.py
+++ pooji30.py
@@ -1,8 +1,8 @@
 hr1,min1=map(int,raw_input().split())
 hr2,min2=map(int,raw_input().split())
-p1=hr1*60+min1
-p2=hr2*60+min2
-diff=abs(p1-p2)
+s1=hr1*60+min1
+s2=hr2*60+min2
+diff=abs(s1-s2)
 time=diff%60
 time1=(diff-time)//60
 print time1,time
