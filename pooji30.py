a940ec493803e1328130587aa653aac12d5d1ff0
Pallapupoojitha <41770542+Pallapupoojitha@users.noreply.github.com>
2018-08-16 15:54:30 +0530

    Update pooji30.py

--- pooji30.py
+++ pooji30.py
@@ -1,5 +1,5 @@
-hr1,min=map(int,raw_input().split())
-hr2,min=map(int,raw_input().split())
+hr1,min1=map(int,raw_input().split())
+hr2,min2=map(int,raw_input().split())
 p1=hr1*60+min1
 p2=hr2*60+min2
 diff=abs(p1-p2)
